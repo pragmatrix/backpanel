ae424bdc55fcc61beeff991dbb846ed38bb4a5f6
Armin Sander <armin@replicator.org>
2017-09-01 10:59:45 +0200

    pack: add distribution to myget

--- pack.sh
+++ pack.sh
@@ -1,4 +1,7 @@
 set -e
 MSBuild.exe BackPanel.sln /t:Clean /p:Configuration=Release
 MSBuild.exe BackPanel.sln /t:BackPanel /p:Configuration=Release
-(cd BackPanel && paket pack .)
+mkdir -p backpanel-nupkg
+rm -f backpanel-nupkg/*.nupkg
+(cd BackPanel && paket pack ../backpanel-nupkg)
+paket push --url https://www.myget.org/F/pragmatrix --endpoint /api/v2/package --api-key $MYGETAPIKEY backpanel-nupkg/*.nupkg
