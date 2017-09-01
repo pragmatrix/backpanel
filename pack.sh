5e47646e917d7fba266a933fd07240b2432d511a
Armin Sander <armin@replicator.org>
2017-09-01 11:00:48 +0200

    change pack directory

--- pack.sh
+++ pack.sh
@@ -1,7 +1,7 @@
 set -e
 MSBuild.exe BackPanel.sln /t:Clean /p:Configuration=Release
 MSBuild.exe BackPanel.sln /t:BackPanel /p:Configuration=Release
-mkdir -p backpanel-nupkg
-rm -f backpanel-nupkg/*.nupkg
-(cd BackPanel && paket pack ../backpanel-nupkg)
-paket push --url https://www.myget.org/F/pragmatrix --endpoint /api/v2/package --api-key $MYGETAPIKEY backpanel-nupkg/*.nupkg
+mkdir -p nupkg
+rm -f nupkg/*.nupkg
+(cd BackPanel && paket pack ../nupkg)
+paket push --url https://www.myget.org/F/pragmatrix --endpoint /api/v2/package --api-key $MYGETAPIKEY nupkg/*.nupkg
