ed04ff28c85a11f85b3838141214ce8609188f5f
Armin Sander <armin@replicator.org>
2017-09-07 16:36:33 +0200

    bump version, use 4 version components again (can't reduce them for myget atm

--- BackPanel/AssemblyInfo.fs
+++ BackPanel/AssemblyInfo.fs
@@ -17,8 +17,8 @@ open System.Runtime.CompilerServices
 
 [<assembly: Guid("d3aa968a-33a1-44bc-9ee9-ff4a0e510c99")>]
 
-[<assembly: AssemblyVersion("0.6")>]
-[<assembly: AssemblyFileVersion("0.6")>]
+[<assembly: AssemblyVersion("0.7.0.0")>]
+[<assembly: AssemblyFileVersion("0.7.0.0")>]
 
 [<assembly: InternalsVisibleTo("BackPanel.Tests")>]
 
